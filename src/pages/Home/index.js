875fd4cb981b4f0ec6b7539638bdb622311f8093
MoonstruckDev <dev@moonstruck.pro>
2024-06-10 11:33:01 +0200

    Fixed Anchor Tags

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -22,7 +22,7 @@ const Page = () => {
       <section className="SliderContainer">
         <Slider />
       </section>
-      <section className="ServicesContainer">
+      <section className="ServicesContainer" id="nos-services">
         <h2 className="Title">Nos services</h2>
         <p>Nous organisons des événements sur mesure partout dans le monde</p>
         <div className="ListContainer">
@@ -51,11 +51,11 @@ const Page = () => {
           </ServiceCard>
         </div>
       </section>
-      <section className="EventsContainer">
+      <section className="EventsContainer" id="nos-realisations">
         <h2 className="Title">Nos réalisations</h2>
         <EventList />
       </section>
-      <section className="PeoplesContainer">
+      <section className="PeoplesContainer" id="notre-equipe">
         <h2 className="Title">Notre équipe</h2>
         <p>Une équipe d’experts dédiés à l’ogranisation de vos événements</p>
         <div className="ListContainer">
@@ -120,8 +120,9 @@ const Page = () => {
           imageSrc={last?.cover || ""}
           title={last?.title || ""}
           date={last ? new Date(last.date) : new Date()}
+          label={last?.type || ""}
           small
-          label="boom"
+
         />
       </div>
       <div className="col contact">
